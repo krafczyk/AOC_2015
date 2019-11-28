4436d33cab12828643214d59bb21aff2e6c47872
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-11-28 14:20:44 -0600

    Solve day 3 task 2

--- day_3.hs
+++ day_3.hs
@@ -5,10 +5,50 @@ import qualified Library.ArgParser as AP
 
 argDefinitions = [ ("input_filepath", ["-i", "--input-file"], "Filepath to use for input", 1) ]
 
+type HLocation = (Int,Int)
+type HMap = Map.Map (Int,Int) Int
+
+move :: Char -> Maybe HLocation -> Maybe HLocation
+move _ Nothing = Nothing
+move dir (Just (x,y))
+    | dir == '^' = Just (x, y+1)
+    | dir == '>' = Just (x+1, y)
+    | dir == '<' = Just (x-1, y)
+    | dir == 'v' = Just (x, y-1)
+    | otherwise = Nothing
+
+updateMap :: Maybe HMap -> Maybe HLocation -> Maybe HMap
+updateMap Nothing _ = Nothing
+updateMap _ Nothing = Nothing
+updateMap (Just hmap) (Just x) = case Map.lookup x hmap of
+                              Nothing -> Just (Map.insert x 1 hmap)
+                              Just y -> Just (Map.insert x (y+1) hmap)
+
+mapFoldFunc :: (Maybe HLocation, Maybe HMap) -> Char -> (Maybe HLocation, Maybe HMap)
+mapFoldFunc (curLoc, hmap) dir = let newLoc = move dir curLoc in
+                                 (newLoc, updateMap hmap $ newLoc)
+
+buildMap :: String -> (Maybe HLocation, Maybe HMap)
+buildMap map_data = foldl mapFoldFunc (Just (0,0), Just (Map.fromList [((0,0),1)])) $ map_data
+
 handleFile :: String -> IO ()
 handleFile input_filepath = withFile input_filepath ReadMode (\handle -> do
                                                                          file_data <- hGetContents handle
-                                                                         -- Read problem data in here)
+                                                                         let problem_data = (lines file_data) !! 0
+                                                                             (_, jhMap) = buildMap problem_data
+                                                                         case jhMap of
+                                                                             Nothing -> putStrLn "ERROR: Didn't build house map correctly!"
+                                                                             Just hMap -> putStrLn $ "Task 1: " ++ (show $ Map.size hMap)
+                                                                         let santa_data = map (fst) $ filter (\(c,idx) -> (idx `mod` 2) == 0) $ zip problem_data [0..]
+                                                                             robo_data = map (fst) $ filter (\(c,idx) -> idx `mod` 2 == 1) $ zip problem_data [0..]
+                                                                             (_, jSantaMap) = buildMap santa_data
+                                                                             (_, jRoboMap) = buildMap robo_data
+                                                                         case (jSantaMap, jRoboMap) of
+                                                                             (Nothing, Nothing) -> putStrLn $ "ERROR: didn't build santa map correctly!"
+                                                                             (Nothing, _) -> putStrLn $ "ERROR: didn't build santa map correctly!"
+                                                                             (_, Nothing) -> putStrLn $ "ERROR: didn't build robo map correctly!"
+                                                                             (Just santaMap, Just roboMap) -> let cMap = Map.unionWith (+) santaMap roboMap in
+                                                                                                              putStrLn $ "Task 2: " ++ (show $ Map.size cMap))
 
 argHelpHandler progArgs args = if AP.helpPresent progArgs args
                                    then AP.writeHelpText progArgs
